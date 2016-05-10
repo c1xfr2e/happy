e54b350bcb91f5849066dbdbdae6f514e4f3c688
gurity <gurity@163.com>
2016-05-11 00:39:15 +0800

    Test tushare!

--- test/use_tushare.py
+++ test/use_tushare.py
@@ -2,7 +2,11 @@
 
 import tushare as ts
 
-code = '600597'
-all_quotes = ts.get_hist_data('600848')
-print all_quotes
+code = '300059'
 
+# 股票历史行情
+all_quotes = ts.get_hist_data(code, start='2016-04-15', ktype='D')
+print all_quotes
+# 前复权日线数据
+fuquan_quotes = ts.get_h_data(code, start='2016-04-15')
+print all_quotes
