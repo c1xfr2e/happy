1d2e17e8d9353c9384033a95626531efddb8afcd
gurity <gurity@163.com>
2016-05-12 10:45:54 +0800

    Try using tushre to get realtime quotes.

--- test/use_tushare.py
+++ test/use_tushare.py
@@ -10,11 +10,17 @@ for i in indices:
     print i
 '''
 
+totol_all = ts.get_today_all()
+
+for i in totol_all.index:
+    row = totol_all.loc[i]
+    print row
+
 fuquan_quotes = ts.get_h_data(code, start='2016-04-15', index=True)
 print fuquan_quotes
 for i in fuquan_quotes.index:
     row = fuquan_quotes.loc[i]
-    print row, type(row)
+    print row.open, row.close
 
 current = fuquan_quotes.loc['2016-05-11']
 print current
