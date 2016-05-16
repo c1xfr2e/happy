4e65f26704bd97f0b1c519a55e59abef92f0e5e5
gurity <gurity@163.com>
2016-05-16 18:31:31 +0800

    Bearychat notification bone.

---
+++ notification/bearychat.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+bearychat_hook_url = 'https://hook.bearychat.com/=bw8wB/incoming/193c8ce496504e8fa04e5303978c7066'
+
+data = {
+    "text": "text, this field may accept markdown",
+    "markdown": True,
+    "channel": "bearychat-dev",
+    "attachments": [
+        {
+            "title": "title_1",
+            "text": "attachment_text",
+            "color": "#ffa500",
+            "images": [
+                {"url": "http://img3.douban.com/icon/ul15067564-30.jpg"}
+            ]
+        }
+    ]
+}
