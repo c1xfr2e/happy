d9acd6d11674cd804e1f5963e51c6bd828b536b0
gurity <gurity@163.com>
2016-05-18 18:15:10 +0800

    Snapshot data model.

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('mysql://root@localhost/alchemist')
+engine = create_engine(
+    'mysql://zh:000000@rdsbi4u1v33wc6zn0w71o.mysql.rds.aliyuncs.com/alchemist?charset=utf8',
+    encoding='utf-8'
+)
 # Base.metadata.bind = engine
 # Session = sessionmaker(bind=engine)
 
@@ -13,28 +16,28 @@ from stock_profile import StockProfile
 from hq_snapshot import HQSnapshot
 
 Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
 
-DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
-session = DBSession()
-
+'''
 snapshot = HQSnapshot(
     code='399006',
     name=u'创业板指',
     date='20160517',
     time='15:00',
     price=2022.38,
-    volume=1000000
+    pre_close=2031.21,
+    open=2000.1,
+    low=1998,
+    high=2088.03,
+    volume=1000000,
+    volume_money=123456789
 )
+
+session = Session()
 session.add(snapshot)
 session.commit()
 
 hq_snapshots = session.query(HQSnapshot).filter(HQSnapshot.code=='399006').all()
 for hq in hq_snapshots:
-    print hq
+    print hq.name, hq.price
+'''
--- models/hq_snapshot.py
+++ models/hq_snapshot.py
@@ -1,16 +1,27 @@
 # coding utf-8
 
-from sqlalchemy import Column, Integer, String, Date, Time, Numeric
+from sqlalchemy import Column, Integer, String, Date, Time, Numeric, BigInteger, UniqueConstraint
 from . import Base
 
 
 class HQSnapshot(Base):
     __tablename__ = 'hq_snapshot'
 
-    id = Column(Integer, primary_key=True)
-    code = Column(String(8), nullable=False)
+    code = Column(String(8), primary_key=True)
+    date = Column(Date, primary_key=True)
+    time = Column(Time, primary_key=True)
     name = Column(String(16))
-    date = Column(Date, nullable=False)
-    time = Column(Time, nullable=False)
     price = Column(Numeric(precision=10, scale=2), nullable=False)
-    volume = Column(Integer, nullable=False)
+    change = Column(Numeric(precision=10, scale=2))
+    change_percent = Column(Numeric(precision=4, scale=2))
+    turnover = Column(Numeric(precision=4, scale=2))
+    pre_close = Column(Numeric(precision=10, scale=2), nullable=False)
+    open = Column(Numeric(precision=10, scale=2), nullable=False)
+    high = Column(Numeric(precision=10, scale=2), nullable=False)
+    low = Column(Numeric(precision=10, scale=2), nullable=False)
+    volume = Column(BigInteger)
+    volume_money = Column(BigInteger, nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint('code', 'date', 'time', name='unique_code_date_time'),
+    )
