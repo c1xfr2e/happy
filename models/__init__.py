ff5eec41a67fc3f7d1c098033013693839b8f42f
gurity <gurity@163.com>
2016-05-21 00:46:17 +0800

    Update hq model.

--- models/__init__.py
+++ models/__init__.py
@@ -4,11 +4,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
-engine = create_engine(
-    'mysql://zh:000000@rdsbi4u1v33wc6zn0w71o.mysql.rds.aliyuncs.com/alchemist?charset=utf8',
-    encoding='utf-8'
+host = 'rdsbi4u1v33wc6zn0w71o.mysql.rds.aliyuncs.com'
+user = 'zh'
+passwd = '000000'
+database = 'alchemist'
+param_charset = 'charset=utf8'
+url = 'mysql://{user}:{passwd}@{host}/{database}?{params}'
+
+aliyun_mysql_url = url.format(
+    host=host, user=user, passwd=passwd, database=database, params=param_charset
 )
+engine = create_engine(aliyun_mysql_url, encoding='utf-8')
+Base = declarative_base()
+
 # Base.metadata.bind = engine
 # Session = sessionmaker(bind=engine)
 
--- models/hq.py
+++ models/hq.py
@@ -7,19 +7,20 @@ from . import Base
 class HQ(Base):
     __tablename__ = 'hq'
 
-    code = Column(String(10), primary_key=True)
     market = Column(String(16), primary_key=True)
+    code = Column(String(10), primary_key=True)
     datetime_from = Column(DateTime, primary_key=True)
     datetime_to = Column(DateTime, primary_key=True)
+
     period = Column(String(16), nullable=False)
     name = Column(String(16))
 
-    pre_close = Column(Numeric(precision=10, scale=3), nullable=False)
     open = Column(Numeric(precision=10, scale=3), nullable=False)
     close = Column(Numeric(precision=10, scale=3), nullable=False)
     low = Column(Numeric(precision=10, scale=3), nullable=False)
     high = Column(Numeric(precision=10, scale=3), nullable=False)
 
+    pre_close = Column(Numeric(precision=10, scale=3), nullable=False)
     change = Column(Numeric(precision=10, scale=3))
     change_percent = Column(Numeric(precision=8, scale=3))
 
