c9eae370f2718c64b89ff8eb75cacb3313aca882
gurity <gurity@163.com>
2016-06-01 23:45:19 +0800

    Try TongLian Data.

---
+++ try/tldata.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import requests
+
+
+token = '27f1ef056e9a5cfdfe3cee5d1c0779e59db0125fc6416010c363d195c10dfdf6'
+url = 'https://api.wmcloud.com/data/v1/api/market/getMktEqudAdj.json'
+headers = {
+    'Authorization': "Bearer " + token,
+    'Accept-Encoding': 'gzip, deflate'
+}
+payload = {
+    'field': '',
+    'beginDate': '20100101',
+    'endDate': '20160601',
+    'secID': '',
+    'ticker': '300342',
+    'tradeDate': ''
+}
+
+r = requests.get(url, headers=headers, params=payload)
+print(r.url)
+print(r.json())
