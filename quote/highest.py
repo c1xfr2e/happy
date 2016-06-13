c1b027d0fb9a0da6665bd31b9363c0e72033e64b
gurity <gurity@163.com>
2016-06-13 16:54:27 +0800

    Find highest at today.

--- quote/highest.py
+++ quote/highest.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+from datetime import date
+
 from sqlalchemy import and_
 import redis
 from models import Session, Stock, Quote
@@ -10,11 +12,6 @@ ss = Session()
 stocks = ss.query(Stock.market, Stock.code).all()
 
 for stock in stocks:
-    key = 'highof20:%s%s' % (stock.market, stock.code)
-    existing = r.hmget(key, 'time')
-    if existing[0]:
-        continue
-
     quotes = ss.query(Quote).filter(
         and_(Quote.market == stock.market, Quote.code == stock.code, Quote.period == 'd1')
     ).order_by(Quote.datetime.desc()).limit(20).all()
@@ -24,4 +21,8 @@ for stock in stocks:
 
     high_quote = max(quotes, key=lambda q: q.high)
 
+    key = 'highof20:%s%s' % (stock.market, stock.code)
     r.hmset(key, {'time': high_quote.datetime, 'high': high_quote.high})
+
+    if high_quote.datetime == date.today():
+        print high_quote.name, high_quote.code
