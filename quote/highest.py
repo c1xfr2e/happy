b0194fedc9eb83d51edeed0b435234b9aa2add0b
gurity <gurity@163.com>
2016-06-13 19:07:50 +0800

    Find high at today.

--- quote/highest.py
+++ quote/highest.py
@@ -1,28 +1,45 @@
 # coding: utf-8
 
-from datetime import date
+from datetime import date, datetime
 
 from sqlalchemy import and_
 import redis
 from models import Session, Stock, Quote
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-
 ss = Session()
-stocks = ss.query(Stock.market, Stock.code).all()
 
-for stock in stocks:
-    quotes = ss.query(Quote).filter(
-        and_(Quote.market == stock.market, Quote.code == stock.code, Quote.period == 'd1')
-    ).order_by(Quote.datetime.desc()).limit(20).all()
+def create_high_20():
+    stocks = ss.query(Stock.market, Stock.code).all()
 
-    if not quotes:
-        continue
+    for stock in stocks:
+        quotes = ss.query(Quote).filter(
+            and_(Quote.market == stock.market, Quote.code == stock.code, Quote.period == 'd1')
+        ).order_by(Quote.datetime.desc()).limit(20).all()
 
-    high_quote = max(quotes, key=lambda q: q.high)
+        if not quotes:
+            continue
 
-    key = 'highof20:%s%s' % (stock.market, stock.code)
-    r.hmset(key, {'time': high_quote.datetime, 'high': high_quote.high})
+        high_quote = max(quotes, key=lambda q: q.high)
 
-    if high_quote.datetime == date.today():
-        print high_quote.name, high_quote.code
+        key = 'highof20:%s%s' % (stock.market, stock.code)
+        r.hmset(key, {'time': high_quote.datetime, 'high': high_quote.high})
+
+        if high_quote.datetime == date.today():
+            print high_quote.name, high_quote.code
+
+
+def find_high_at_today():
+    stocks = ss.query(Stock).all()
+    for stock in stocks:
+        key = 'highof20:%s%s' % (stock.market, stock.code)
+        high_time = r.hmget(key, 'time')
+        if high_time[0] is None:
+            continue
+        high_date = datetime.strptime(high_time[0], '%Y-%m-%d %H:%M:%S').date()
+        if high_date == date.today():
+            print stock.code, stock.name
+
+
+if __name__ == '__main__':
+    find_high_at_today()
