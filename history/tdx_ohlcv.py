b713dbf2d8b1a1e8207ce171bff25ece18812bae
gurity <gurity@163.com>
2016-07-02 13:49:25 +0800

    TDX

--- history/tdx_ohlcv.py
+++ history/tdx_ohlcv.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 from datetime import datetime
+from decimal import Decimal
+from models.history_quote import model_of_quote
 
 f_ohlcv = open('../data/history_ohlcv/600036.txt', 'rb')
 
@@ -8,9 +10,15 @@ while True:
     line = f_ohlcv.readline().decode('gbk').rstrip('\r\n')
     if not line:
         break
-    datas = line.split(';')
-    date = datetime.strptime(datas[0], '%Y%m%d').date()
-
-    if len(datas) != 7:
+    data = line.split(';')
+    if len(data) != 7:
         break
-    print datas
+
+    date = datetime.strptime(data[0], '%Y%m%d').date()
+    open = Decimal(data[1])
+    high = Decimal(data[2])
+    low = Decimal(data[3])
+    close = Decimal(data[4])
+    volume = int(data[5])
+    amount = int(data[6])
+
